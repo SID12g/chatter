194d4e0414bc8bb09f443ea1c58d834f5fb6d757
SID12g <sid12g@naver.com>
2024-05-15 11:46:26 +0900

    feat: auth system

---
+++ src/next-auth.d.ts
@@ -0,0 +1,12 @@
+import { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface User {
+    id?: string;
+    username?: string;
+  }
+
+  interface Session extends DefaultSession {
+    user?: User;
+  }
+}
