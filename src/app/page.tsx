eaa24c4e96483e8a55b43215121038a98c1f5b83
SID12g <sid12g@naver.com>
2024-05-15 12:33:47 +0900

    feat: search chatting group

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,17 @@ import Signout from "@/components/signout/signout";
 import Signup from "@/components/signup/signup";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import { connectDB } from "@/utils/database";
+import Link from "next/link";
 
 export default async function Home() {
   const session: any = await getServerSession(authOptions);
+  let db = (await connectDB).db("chat");
+  let chats = await db
+    .collection("group")
+    .find({ users: { $in: [session.user.user._id.toString()] } })
+    .toArray();
+  console.log(chats);
   return (
     <main className={styles.main}>
       <div style={{ display: "flex" }}>
@@ -19,6 +27,24 @@ export default async function Home() {
             <Signin /> <Signup />
           </div>
         )}
+      </div>
+      <div>
+        <form method="POST" action="/api/chatting/add">
+          <input name="withchat" />
+          <input
+            defaultValue={session.user.user._id.toString()}
+            name="maker"
+            style={{ display: "none" }}
+          />
+          <button>와 채팅방 만들기</button>
+        </form>
+        <div>
+          {chats.map((a, i) => (
+            <Link key={i} href={`/chat/${a._id.toString()}`}>
+              {a._id.toString()}
+            </Link>
+          ))}
+        </div>
       </div>
     </main>
   );
